35d79c8620de56383f9400d61be0bc7d7c9caf4e
Vincenzo Ruggiero <vincenzo@shopigr.am>
2014-12-18 11:51:55 +0100

    Use a personnal token

--- lib/doorkeeper/oauth/helpers/unique_token.rb
+++ lib/doorkeeper/oauth/helpers/unique_token.rb
@@ -5,7 +5,7 @@ module Doorkeeper
         def self.generate(options = {})
           generator_method = options.delete(:generator) || SecureRandom.method(:hex)
           token_size       = options.delete(:size)      || 32
-          generator_method.call(token_size)
+          "pp_api_key_#{generator_method.call(token_size)}"
         end
       end
     end
