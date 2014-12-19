3496deac22acb84a2e917a130cccacdc4e4a128c
Vincenzo Ruggiero <vincenzo@shopigr.am>
2014-12-19 09:47:18 +0100

    No prefix for tokens

--- lib/doorkeeper/oauth/helpers/unique_token.rb
+++ lib/doorkeeper/oauth/helpers/unique_token.rb
@@ -5,7 +5,7 @@ module Doorkeeper
         def self.generate(options = {})
           generator_method = options.delete(:generator) || SecureRandom.method(:hex)
           token_size       = options.delete(:size)      || 32
-          "pp_api_key_#{generator_method.call(token_size)}"
+          generator_method.call(token_size)
         end
       end
     end
